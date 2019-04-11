a8b074705579d57b3a219dd55d6f8d9bf78060b9
Bernadette Estacio <bernadette@englemaninvest.com>
2019-04-10 22:23:36 -0400

    Move data to own file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { getQuotes } from "./quotes";
 import cookie from "./img/cookie.jpg";
 
 class App extends Component {
   state = {
+    quotes: getQuotes(),
     quote: null
   };
 
@@ -10,7 +12,7 @@ class App extends Component {
     setTimeout(
       function() {
         this.setState({
-          quote: this.props.quotes[Math.floor(Math.random() * 20)]
+          quote: this.state.quotes[Math.floor(Math.random() * 20)]
         });
       }.bind(this),
       250
@@ -46,46 +48,16 @@ class App extends Component {
   }
 }
 
-const MainContent = props => {
+const MainContent = ({ getQuote, quote }) => {
   return (
     <main>
       <h1>Click Fortune Cookie</h1>
 
-      <img
-        src={cookie}
-        alt="fortune cookie"
-        width="200"
-        onClick={props.getQuote}
-      />
+      <img src={cookie} alt="fortune cookie" width="200" onClick={getQuote} />
 
-      <p>{props.quote}</p>
+      <p>{quote}</p>
     </main>
   );
-};
-
-App.defaultProps = {
-  quotes: [
-    "A smile is your passport into the hearts of others.",
-    "What ever your goal is in life, embrace it, visualize it, and for it will be yours.",
-    "You cannot love life until you live the life you love.",
-    "The man or woman you desire feels the same about you.",
-    "Our deeds determine us, as much as we determine our deeds.",
-    "You can make your own happiness.",
-    "A stranger is a friend you have not spoken to yet.",
-    "A journey of a thousand miles begins with a single step.",
-    "Believing that you are beautiful will make you appear beautiful to others around you.",
-    "If you refuse to accept anything but the best, you very often get it.",
-    "Failure is only the opportunity to begin again more intelligently.",
-    "Integrity is doing the right thing, even if nobody is watching.",
-    "Conquer your fears or they will conquer you.",
-    "In this life it is not what we take up, but what we give up, that makes us rich.",
-    "A single conversation with a wise man is better than ten years of study.",
-    "The world may be your oyster, but that doesn't mean you'll get it's pearl.",
-    "Do not follow where the path may lead. Go where there is no path...and leave a trail.",
-    "We can't help everyone. But everyone can help someone.",
-    "All the water in the world can't sink a ship unless it gets inside.",
-    "Generosity will repay itself sooner than you imagine."
-  ]
 };
 
 export default App;
---
+++ src/quotes.js
@@ -0,0 +1,26 @@
+const quotes = [
+  "A smile is your passport into the hearts of others.",
+  "What ever your goal is in life, embrace it, visualize it, and for it will be yours.",
+  "You cannot love life until you live the life you love.",
+  "The man or woman you desire feels the same about you.",
+  "Our deeds determine us, as much as we determine our deeds.",
+  "You can make your own happiness.",
+  "A stranger is a friend you have not spoken to yet.",
+  "A journey of a thousand miles begins with a single step.",
+  "Believing that you are beautiful will make you appear beautiful to others around you.",
+  "If you refuse to accept anything but the best, you very often get it.",
+  "Failure is only the opportunity to begin again more intelligently.",
+  "Integrity is doing the right thing, even if nobody is watching.",
+  "Conquer your fears or they will conquer you.",
+  "In this life it is not what we take up, but what we give up, that makes us rich.",
+  "A single conversation with a wise man is better than ten years of study.",
+  "The world may be your oyster, but that doesn't mean you'll get it's pearl.",
+  "Do not follow where the path may lead. Go where there is no path...and leave a trail.",
+  "We can't help everyone. But everyone can help someone.",
+  "All the water in the world can't sink a ship unless it gets inside.",
+  "Generosity will repay itself sooner than you imagine."
+];
+
+export function getQuotes() {
+  return quotes;
+}
